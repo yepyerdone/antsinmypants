a18174f4053ced1bab56ae1e14bcea005753f06d
yepyerdone <crazygary619@gmail.com>
2026-05-01 23:23:03 -0400

    Update MolarMadness.tsx
    removed . in import

--- blackjack-99_-battle-royale/src/games/molar-madness/MolarMadness.tsx
+++ blackjack-99_-battle-royale/src/games/molar-madness/MolarMadness.tsx
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect, useState, useCallback } from 'react';
 import { Tile, TILE_SIZE, COLORS, COLORS_L2, INITIAL_MAZE, INITIAL_MAZE_2, GRID_WIDTH, GRID_HEIGHT } from '../game/constants';
 import { motion, AnimatePresence } from 'motion/react';
-import { db } from '../lib/firebase';
+import { db } from './lib/firebase';
 import { 
   collection, 
   query, 
