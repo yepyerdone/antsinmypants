387810b522a09f7de83c117b4d6cde19f3d2c802
yepyerdone <crazygary619@gmail.com>
2026-05-03 23:36:57 -0400

    Update firebase.ts

--- blackjack-99_-battle-royale/src/games/molar-madness/lib/firebase.ts
+++ blackjack-99_-battle-royale/src/games/molar-madness/lib/firebase.ts
@@ -1,8 +1,1 @@
-import { initializeApp, getApps, getApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import firebaseConfig from '../firebase-applet-config.json';
-
-// Prevent duplicate Firebase initialization
-const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
-
-export const db = getFirestore(app);
+export { db } from '../../../lib/firebase';
